264b581f910bc8a833f2b49c794f67438dfb06e5
Z000mg <zooomg2015@yandex.ru>
2020-11-15 13:26:11 +0300

    final

--- src/test/java/FillFormLesson2.java
+++ src/test/java/FillFormLesson2.java
@@ -8,6 +8,9 @@ import javax.swing.*;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FillFormLesson2 {
@@ -19,7 +22,7 @@ public class FillFormLesson2 {
             email = "exmpl@ya.ru",
             number = "9031234567",
             adress = "Lenina str.",
-            subj = "WTF";
+            subj = "Physics";
     File file = new File("src/test/resources/1.jpg");
 
     @BeforeAll
@@ -30,7 +33,7 @@ public class FillFormLesson2 {
     @Test
     void lesson2Form() {
         open ("https://demoqa.com/automation-practice-form");
-        $("html").shouldHave(Condition.text("Student Registration Form"));
+        $("html").shouldHave(text("Student Registration Form"));
         $("#firstName").val(first_name);
         $("#lastName").val(last_name);
         $("#userEmail").val(email);
@@ -41,34 +44,24 @@ public class FillFormLesson2 {
         $(".react-datepicker__year-select").selectOptionByValue("1973");
         $(".react-datepicker__day--007").click();
         $("#subjectsInput").click();
-        $("#subjectsInput").val(subj);
+        $("#subjectsInput").val(subj).pressEnter();
         $x("//*[@for='hobbies-checkbox-1']").click();
         $x("//*[@for='hobbies-checkbox-3']").click();
         $("#uploadPicture").uploadFile(file);
         $("#currentAddress").val(adress);
-        $("#stateCity-wrapper").click();
+        $("#stateCity-wrapper").$(byText("Select State")).click();
+        $(byText("NCR")).click();
+        $("#stateCity-wrapper").$(byText("Select City")).click();
+        $(byText("Delhi")).click();
+        $("#submit").click();
 
-        $x("//*[@for='hobbies-checkbox-3']").click();
-
+        $x("//td[text()='Student Name']/following::td[1]").shouldHave(text(first_name + " " + last_name));
+        $x("//td[text()='Student Email']/following::td[1]").shouldHave(text(email));
+        $x("//td[text()='Gender']/following::td[1]").shouldHave(text("Male"));
+        $x("//td[text()='Mobile']/following::td[1]").shouldHave(text(number));
+        $x("//td[text()='Hobbies']/following::td[1]").shouldHave(text("Sports, Music"));
+        $x("//td[text()='Picture']/following::td[1]").shouldHave(text("1.jpg"));
+        $x("//td[text()='Address']/following::td[1]").shouldHave(text(adress));
+        $x("//td[text()='State and City']/following::td[1]").shouldHave(text("NCR Delhi"));
     }
-
 }
-
-
-
-// найти Student Registration Form
-// заполнить first name
-// заполнить last name
-// заполнить email
-// выбрать gender
-// ввести номер (10 цифр)
-// выбрать дату
-// написать что-то в subjects
-// выбрать хобби
-// выбрать и загрузить картинку
-// заполнить адрес
-// выбрать хобби
-// выбрать штат
-// выбрать город
-// нажать сабмит
-// проверить что выдано
